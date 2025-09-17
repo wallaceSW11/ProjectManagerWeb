ed0201cf95e2901028b84033fab5e269de663c8b
wallace.dsn.erp <wallace.dsn.erp@alterdata.com.br>
2025-09-17 11:39:10 -0300

    Ajuste na ordenação da ordem de execucao

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,35 +1,29 @@
 using System.Text.Json.Serialization;
-using Microsoft.Extensions.FileProviders;
 using ProjectManagerWeb.src.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    WebRootPath = "frontend"
+});
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-// Adiciona política de CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll",
-        builder =>
-        {
-            builder
-                .AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader();
-        });
+    // Dica: Para produção, troque por .WithOrigins("http://seu-dominio.com")
+    options.AddPolicy("AllowAll", policy => 
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 });
 
 builder.Services.AddControllers().AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
-        options.JsonSerializerOptions.MaxDepth = 32;
-    });
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
+    options.JsonSerializerOptions.MaxDepth = 32;
+});
 
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSingleton<RepositorioJsonService>();
 builder.Services.AddSingleton<ConfiguracaoService>();
 builder.Services.AddSingleton<CloneService>();
@@ -44,29 +38,11 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-// habilita arquivos estáticos
-app.UseDefaultFiles(); // para servir index.html por padrão
-app.UseStaticFiles(new StaticFileOptions
-{
-    FileProvider = new PhysicalFileProvider(
-        Path.Combine(Directory.GetCurrentDirectory(), "frontend")
-    ),
-    RequestPath = ""
-});
-
-// se não encontrar rota na API, cai pro index.html do Vue
-app.MapFallbackToFile("index.html", new StaticFileOptions
-{
-    FileProvider = new PhysicalFileProvider(
-        Path.Combine(Directory.GetCurrentDirectory(), "frontend")
-    )
-});
-
 // Habilita CORS
 app.UseCors("AllowAll");
-
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
